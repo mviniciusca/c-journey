a65393391f1012189074ca2e287e4686cdd5e982
Marcos Coelho <mviniciusca@gmail.com>
2024-11-15 13:20:47 -0300

    wip

---
+++ media.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 9;
+    int b = 8;
+    int c = 10;
+    int d = 5;
+
+    int media = (a + b + c + d) / 4;
+
+    printf("%d", media);
+}
---
+++ sum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 10;
+    int y = 5;
+
+    int sum = x + y;
+
+    printf("%d", sum);
+}
